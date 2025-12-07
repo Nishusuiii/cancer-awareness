240b23aa812f1ae020d35146c71ca52335ab55a9
Nishusuiii <27nishushetty@gmail.com>
2025-12-07 23:11:33 +0530

    Add files via upload

---
+++ cancer-awareness-site/script.js
@@ -0,0 +1,39 @@
+// MAIN FUNCTION TO LOAD QUOTE
+async function loadQuote() {
+    const quoteText = document.getElementById("quote-text");
+    const quoteAuthor = document.getElementById("quote-author");
+
+    quoteText.innerHTML = "“Loading quote...”";
+    quoteAuthor.innerHTML = "—";
+
+    try {
+       
+        const res = await fetch("https://api.quotable.io/random");
+
+        if (!res.ok) throw new Error("Quotable API failed");
+
+        const data = await res.json();
+
+        quoteText.innerHTML = `“${data.content}”`;
+        quoteAuthor.innerHTML = `— ${data.author}`;
+    } 
+    catch (error) {
+        console.warn("Quotable API failed, switching to backup!", error);
+
+        try {
+            const res2 = await fetch("https://zenquotes.io/api/random");
+
+            const data2 = await res2.json();
+
+            quoteText.innerHTML = `“${data2[0].q}”`;
+            quoteAuthor.innerHTML = `— ${data2[0].a}`;
+        } 
+        catch (err2) {
+            console.error("Both APIs failed!", err2);
+            quoteText.innerHTML = "Couldn't load quote 😢";
+            quoteAuthor.innerHTML = "";
+        }
+    }
+}
+
+window.onload = loadQuote;
